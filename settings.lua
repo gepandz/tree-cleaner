dc332a811ca402d2f85b6a9d2ddaaffe15146991
GPS <gepandz@gmail.com>
2020-04-09 20:55:13 -0700

    Add required setting_type parameter

--- settings.lua
+++ settings.lua
@@ -2,6 +2,7 @@ data:extend({
     {
         type = "int-setting",
 	name = "tree-cleaner-chunk-range",
+	setting_type  = "runtime-global",
 	default_value = 3,
 	minimum_value = 0,
 	maximum_value = 10
