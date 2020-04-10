68219db6563bde343d28ab02aa042f52275f46ec
GPS <gepandz@gmail.com>
2020-04-09 20:14:37 -0700

    Attempt to clean up performance

--- control.lua
+++ control.lua
@@ -3,7 +3,7 @@
 script.on_event({defines.events.on_tick},
    function (e)
       if e.tick % 30 == 0 then -- 60 ticks per second, so check every half-second
-         for index,tree in pairs(game.surfaces["nauvis"].find_entities_filtered{type="tree"}) do
+         for index,tree in pairs(game.surfaces["nauvis"].find_entities_filtered{type="tree", limit=50}) do
             if tree.tree_stage_index == tree.tree_stage_index_max and not(tree.to_be_deconstructed()) then
                tree.order_deconstruction(game.forces["player"])
             end
